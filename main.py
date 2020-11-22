cc7d6129f0a9e494be0dcab2db0385b808db334d
Peyton Fitzgerald <pdf272@gmail.com>
2020-11-22 01:02:27 -0500

    before bed

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from random import randint, shuffle
 import numpy as np
 
+def check_board(board):
+    pass
+
 def create_board(num):
-    board = [['' for _ in range(num)] for _ in range(num)]
+    board = [[' ' for _ in range(num+1)] for _ in range(num+1)]
     print(np.matrix(board))
     board_size = num^2
     num_options = []
 
+    for column in range(1, len(board[0])):
+        board[0][column] = chr(96+column)
+
+    
+    for row in range(1, len(board)):
+        board[row][0] = chr(64+row)
+
+    
+    print(np.matrix(board))
+
+    """
     for opt in range(0,num):
         num_options.append(opt)
     options_size = len(num_options)
@@ -15,7 +29,7 @@ def create_board(num):
         y = n%options_size
         if board[x][y]=='':
             for opt in num_options:
-                
+    """   
         
     
     # board = [[random.random() for i in range(sudoku_number)] for j in range(sudoku_number)]
